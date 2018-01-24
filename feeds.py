3584567d244d6d3fb3f6103a4344a325bd5150f1
chawdhry <32017923+chawdhry@users.noreply.github.com>
2018-01-24 17:06:59 +0530

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -1,16 +1,8 @@
 FEEDS = [
-    {
-        'title': 'Apple news',
-        'route': 'apple-news',
-        'url': 'https://www.google.com/alerts/feeds/16328023999629469583/8065899985326555180'
-    }, {
+  {
         'title': 'Bitcoin News',
         'route': 'google-news',
         'url': 'https://www.google.com/alerts/feeds/05370028073045496411/12780294917687067322'
-    }, {
-         'title': 'Tesla news',
-         'route': 'tesla-news',
-         'url': 'https://www.google.com/alerts/feeds/16328023999629469583/11697916327024296501'
-     }
+    }
 ]
 
