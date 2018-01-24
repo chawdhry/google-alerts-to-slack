fcde4a34c3511fb9c00be8f459f25c747c029252
chawdhry <32017923+chawdhry@users.noreply.github.com>
2018-01-24 17:13:59 +0530

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -1,6 +1,6 @@
 FEEDS = [
   {
-        'title': 'Bitcoin News',
+        'title': 'google-news',
         'route': 'google-news',
         'url': 'https://www.google.com/alerts/feeds/05370028073045496411/12780294917687067322'
     }
